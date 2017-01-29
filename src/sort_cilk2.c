04b8dda0246048138a481dc27862937ba08e8302
moritzmacke <moritz.macke@gmx.at>
2017-01-30 00:22:13 +0100

    Cleanup

--- src/sort_cilk2.c
+++ src/sort_cilk2.c
@@ -40,8 +40,6 @@ static size_t umax(size_t a, size_t b) {
 }
 
 
-
-
 static void swap_elem(char *a, char *b, size_t size) {
 
     #ifdef SEQUENTIAL
@@ -137,101 +135,77 @@ static size_t findPrefix(CHUNK chunks[], uint32_t lo, uint32_t range, size_t lef
 static void partitionPass1(char *array, ITYPE *type, size_t length, CHUNK chunks[], uint32_t loChunk, uint32_t range, uint32_t chunksize, void *pivot) {
 	
 	if(range > 1) {
-		cilk_spawn partitionPass1(array, type, length, chunks, loChunk, range/2, chunksize, pivot);
-		cilk_spawn partitionPass1(array, type, length, chunks, loChunk + range/2, range - (range/2), chunksize, pivot);
+	  cilk_spawn partitionPass1(array, type, length, chunks, loChunk, range/2, chunksize, pivot);
+	  cilk_spawn partitionPass1(array, type, length, chunks, loChunk + range/2, range - (range/2), chunksize, pivot);
     
-    cilk_sync;
+      cilk_sync;
 
-		chunks[loChunk + range].partial_pf_le += chunks[loChunk + (range/2)].partial_pf_le; //careful here to avoid race...
+	  chunks[loChunk + range].partial_pf_le += chunks[loChunk + (range/2)].partial_pf_le; //careful here to avoid race...
 	}
 	else {
-
-//    printf("pass1 body\n");
-
-		size_t start = loChunk*chunksize;
-    size_t chunklen = (start+chunksize < length) ? chunksize : length - start;
+	  size_t start = loChunk*chunksize;
+      size_t chunklen = (start+chunksize < length) ? chunksize : length - start;
 			
-		chunks[loChunk].startIndex = start;
-    chunks[loChunk].count_total = chunklen;
+	  chunks[loChunk].startIndex = start;
+      chunks[loChunk].count_total = chunklen;
 
-//    printf("Pass1: start: %lu len: %lu\n", start, chunklen);
+      size_t step = type->size;
+	  long left = start;
+	  long right = start + chunklen - 1;
 
-    size_t step = type->size;
-			
-		long left = start;
-		long right = start + chunklen - 1;
-    
-//    char *pstart = &array[start*step];
-//    char *pleft = pstart;
-//    char *pright = &pstart[right*step];
+	  if(loChunk % 2 == 0) {
+		//le left, gt right
+	    for(;;) {
+	      while(left <= right && (type->compare(&array[left*step], pivot) <= 0) ) {
+	        left++;
+	      }
+	      while(right >= left && (type->compare(&array[right*step], pivot) > 0) ) {
+	        right --;
+	      }
+	      if(left > right) {
+	        break;
+	      }
+	      else {
 
-		if(loChunk % 2 == 0) {
-			//le left, gt right
-			for(;;) {
-				while(left <= right && (type->compare(&array[left*step], pivot) <= 0) ) {
-					left++;
-				}
-				while(right >= left && (type->compare(&array[right*step], pivot) > 0) ) {
-					right --;
-				}
-				if(left > right) {
-					break;
-				}
-				else {
-//          printf("Pass1: swap %ld <-> %ld \n", left, right);
-#ifdef SEQUENTIAL
-          pass1_swaps++;
-#endif
+    #ifdef SEQUENTIAL
+            pass1_swaps++;
+    #endif
 
-					swap_elem(&array[left*step], &array[right*step], step);
-          left++;
-          right--;
-				}
-			}
-
-//      right = (pright - (char *) array)/step;
-
-			size_t countLE = right - start + 1;
-
-			chunks[loChunk+1].partial_pf_le = countLE;
-			chunks[loChunk].count_le = countLE;
-				
-			chunks[loChunk].split = right + 1;
+	        swap_elem(&array[left*step], &array[right*step], step);
+            left++;
+            right--;
+	      }
+	    }
+	      size_t countLE = right - start + 1;
+	      chunks[loChunk+1].partial_pf_le = countLE;
+	      chunks[loChunk].count_le = countLE;	
+	      chunks[loChunk].split = right + 1;
 		}
 		else {
 			//gt left, le right
-			for(;;) {
-				while(left <= right && (type->compare(&array[left*step], pivot) > 0) ) {
-					left++;
-				}
-				while(right >= left && (type->compare(&array[right*step], pivot) <= 0) ) {
-					right --;
-				}
-				if(left > right) {
-					break;
-				}
-				else {
-//          printf("Pass1: swap %ld <-> %ld \n", left, right);
-
-#ifdef SEQUENTIAL
-          pass1_swaps++;
-#endif
-
-					swap_elem(&array[left*step], &array[right*step], step);
-          left++;
-          right--;
-				}
-			}
-
-//      right = (pright - (char *) array)/step;
-//      left = (pleft - (char *) array)/step;
-				
-			size_t countLE = start + chunklen - right - 1;
-
-			chunks[loChunk+1].partial_pf_le = countLE;
-			chunks[loChunk].count_le = countLE;
-				
-			chunks[loChunk].split = left;
+	      for(;;) {
+	        while(left <= right && (type->compare(&array[left*step], pivot) > 0) ) {
+	          left++;
+	        }
+	        while(right >= left && (type->compare(&array[right*step], pivot) <= 0) ) {
+	          right --;
+	        }
+	        if(left > right) {
+	          break;
+	        }
+	        else {
+    #ifdef SEQUENTIAL
+              pass1_swaps++;
+    #endif
+	          swap_elem(&array[left*step], &array[right*step], step);
+              left++;
+              right--;
+	        }
+	      }
+	      size_t countLE = start + chunklen - right - 1;
+	      chunks[loChunk+1].partial_pf_le = countLE;
+	      chunks[loChunk].count_le = countLE;	
+	      chunks[loChunk].split = left;
 		}
 	}
 }
@@ -239,21 +213,18 @@ static void partitionPass1(char *array, ITYPE *type, size_t length, CHUNK chunks
 
 static void partitionPass2(char *array, ITYPE *type, size_t length, CHUNK chunks[], uint32_t numChunks, size_t swapIndex, size_t swapCount, size_t targetOffset, uint32_t chunksize) {
 
-//  printf("SwapCount: %lu\n", swapCount);
-
-  if(swapCount == 0)
-    return;
+    if(swapCount == 0) {
+        return;
+    }
 
 	if(swapCount > umax(chunksize/2, 1000) /*LIMIT*/) { //swaps to do per proc, set to what?
 		cilk_spawn partitionPass2(array, type, length, chunks, numChunks, swapIndex, swapCount/2, targetOffset, chunksize);
 		cilk_spawn partitionPass2(array, type, length, chunks, numChunks, swapIndex + swapCount/2, swapCount - swapCount/2, targetOffset, chunksize);
-    
 	}
 	else {
 	
 		size_t srcIndex = findSwapSrc(chunks, 0, numChunks, swapIndex, 0);
 		size_t tgtIndex = findSwapTgt(chunks, 0, numChunks, swapIndex+targetOffset, 0);
-		
 		
 		uint32_t iSrcRunEnd = (srcIndex/chunksize) | 0x1;	//always ends in uneven block except last, but that one can't be source
 		size_t srcRunEnd = chunks[iSrcRunEnd].split;
@@ -292,58 +263,37 @@ static void partitionPass2(char *array, ITYPE *type, size_t length, CHUNK chunks
 #ifdef SEQUENTIAL
           pass2_swaps += swapLen;
 #endif
-
-
 			memswap(&array[srcIndex*type->size], &array[tgtIndex*type->size], swapLen * type->size);     
 
 			srcIndex += swapLen;
 			tgtIndex += swapLen;
-
-			
 			swapCount -= swapLen;
 		}
-
 	}
 }
 
 static size_t parallelPartition(void *array, ITYPE *type, size_t length, uint32_t chunksize, void *pivot) {
-	
-//  printf("Pivot: %d\n", *((int *)pivot));
-//  print_vals(array, type, length);
 
 	uint32_t numChunks = (length + chunksize - 1)/chunksize;
 	
 	CHUNK *chunks = malloc((numChunks+1)*sizeof(CHUNK));
 
-	//void *a, ITYPE *, size_t length, CHUNK *, size_t loChunk, size_t range, size_t chunksize, void *pivot
-
-  //init first pf
-  chunks[0].partial_pf_le = 0;
-
-//  printf("Before pass1\n");
+    chunks[0].partial_pf_le = 0;
 
 	partitionPass1((char *) array, type, length, chunks, 0, numChunks, chunksize, pivot);
 
-
-//  printf("After pass1\n");
-//  print_vals(array, type, length);
-	
 	chunks[numChunks].startIndex = length;
-  chunks[numChunks].count_total = 0;
+    chunks[numChunks].count_total = 0;
 	chunks[numChunks].split = length;
 
-//  printf("After prefixSums\n");
-//  print_chunks(chunks, numChunks);
-	
 	size_t countLE = chunks[numChunks].partial_pf_le; //complete for last block
+
 	if(countLE < length && countLE > 0) {	//all le or all gt?
 		uint32_t iSplitChunk = countLE/chunksize;
 		size_t splitChunkOffset = countLE - iSplitChunk*chunksize;
 		CHUNK *splitChunk = &chunks[iSplitChunk];
 		
 		size_t swapCount = chunksize*iSplitChunk - findPrefix(chunks, 0, numChunks, 0, iSplitChunk);
-
-//    printf("swaps before splitblock: %lu\n", swapCount);
 		
 		if(iSplitChunk % 2 == 1) {
 			swapCount += umin(splitChunk->split - splitChunk->startIndex, splitChunkOffset);
@@ -352,14 +302,7 @@ static size_t parallelPartition(void *array, ITYPE *type, size_t length, uint32_
 			swapCount += splitChunkOffset - splitChunk->count_le;
 		}
 
-//    printf("Swaps: %lu\n", swapCount);
-
-    //divide up swaps
-    //void *a, ITYPE *, size_t len, CHUNK *, size_t numChunks, size_t swapIndex, size_t swapCount, size_t targetOffset
 		partitionPass2((char *) array, type, length, chunks, numChunks, 0, swapCount, countLE-swapCount, chunksize);
-
-//    printf("After pass2\n");
-//    print_vals(array, type, length);
 	}
 
   free(chunks);
@@ -367,131 +310,39 @@ static size_t parallelPartition(void *array, ITYPE *type, size_t length, uint32_
   return countLE;
 }
 
-//this seems to cause very bad behaviour in case of reverse sorted array
-/*
-static void *selectPivot(void *array, ITYPE *type, size_t length) {
-  void *first = array;
-  void *mid = (void *) (((char *)array) + (length/2)*type->size);
-  void *last = (void *) (((char *)array) + (length-1)*type->size);
-  void *pivot = first;
-
-//  printf("Pivot candidates: %d, %d, %d\n", *((int *) first) , *((int *) mid), *((int *) last));
-
-  if(type->compare(first, last) > 0) {
-    //first > last
-    if(type->compare(first, mid) > 0) {
-      //first > last, mid
-      if(type->compare(mid, last) > 0) {
-        //first > mid > last
-        pivot = mid;
-      }
-      else {
-        //first > last >= mid
-        pivot = last;
-      }
-    }
-    else {
-      //mid >= first > last
-    }
-  }
-  else {
-    //last >= first
-    if(type->compare(first, mid) > 0) {
-      //last >= first > mid
-    }
-    else {
-      //last, mid >= first
-      if(type->compare(last, mid) > 0) {
-        //last > mid >= first
-        pivot = mid;
-      }
-      else {
-        //mid >= last >= first
-        pivot = last;
-      }
-    }
-  }
-
-//  printf("Chose: %d\n", *((int *) pivot));
-
-  return pivot;
-}
-*/
-
 static void qs(void *array, ITYPE *type, size_t length, uint32_t chunksize, int32_t rstate) {
 
 #ifdef SEQUENTIAL
     qs_calls++;
 #endif
 
-//  printf("\nQS: length: %lu\n", length);
-
-//  sleep(1);
-
   if(length > 2*chunksize) {
-
-//    printf("\n");
-//    print_ints(items, size);
-
     char *a = (char *) array;
 
-
-//    void *pivot = selectPivot(array, type, length);
-//    void *pivot = &a[(rand()%length)*type->size];
     void *pivot = selectPivotRandom(array, type, length, 7, &rstate);
-
-
-
     swap_elem(&a[0], (char *) pivot, type->size); //swap pivot to beginning
     pivot = array;
 
     //printf("Pivot: %d\n", *pivot);
 
     size_t size_left = parallelPartition(&a[type->size], type, length-1, chunksize, pivot);
-
     size_t size_right = length - size_left - 1;
-
-//    printf("Split at %lu\n", size_left);
-
-//    printf("Split: %.2f%%\n", 100.0*size_left/(double)length);
-
 
     swap_elem((char *) pivot, &a[size_left*type->size], type->size); //swap pivot in place
 
-
-//    if(size_left > chunksize) {
-      cilk_spawn qs(array, type, size_left, chunksize, rstate);
-/*    }
-    else {
-      qsort(array, size_left, type->size, type->compare);
-      //parallelQuicksort(array, type, size_left, chunksize);
-    }
-
-    if(size_right > chunksize) {
-*/      cilk_spawn qs(&a[(size_left+1)*type->size], type, size_right, chunksize, rstate);
-/*    }
-    else {
-      qsort(&a[(size_left+1)*type->size], size_right, type->size, type->compare);
-//     parallelQuicksort(&a[(size_left+1)*type->size], type, size_right, chunksize);
-    }
-*/
-
+    cilk_spawn qs(array, type, size_left, chunksize, rstate);
+    cilk_spawn qs(&a[(size_left+1)*type->size], type, size_right, chunksize, rstate);
   }
   else {
     qsort(array, length, type->size, type->compare);
   }
-
 }
 
 void parallelQuicksort(void *array, ITYPE *type, size_t length, size_t chunksize) {
-
-//  time_t now = time(NULL);
-//  srand(now);
   
   qs(array, type, length, chunksize, 612345789);
 #ifdef SEQUENTIAL
   printf("Total swaps: %lu, pass1: %lu, pass2: %lu\n", total_swaps, pass1_swaps, pass2_swaps);
   printf("Quicksort calls: %lu\n", qs_calls);
 #endif
-
 }
--- src/sort_omp.c
+++ src/sort_omp.c
@@ -7,14 +7,9 @@
 #include "sort.h"
 
 typedef struct _chunk {
-//  int thread_id;
-//  int thread_id2;
   size_t startIndex;
   size_t count_total;
   size_t count_le;
-//  size_t prefix_le;
-//  size_t count_gt;
-//  size_t count_eq;
 } CHUNK;
 
 /* */
@@ -87,23 +82,13 @@ void partition_task(char **p_left, char **p_right, char *pivot, size_t size, FP_
 void qs_task(char *start, ITYPE *type, size_t length) {
   if(length > 1000/*CUTOFF*/) {
 
-//    printf("Qs_task (%lu), thread %d\n", length, omp_get_thread_num());
-
     char *end = &start[(length-1)*type->size];
     char *left = start;
     char *right = end;
 
     char *pivot = selectPivotTask(&left, &right, type->size, type->compare);
 
-//    printf("Qs_task (%lu) Pivot: ", length);
-//    print_val(pivot, type);
-//    printf("\n");
-//    print_vals(start, type, length);
-
     partition_task(&left, &right, pivot, type->size, type->compare);
-
-//    printf("Qs_task partition\n");
-//    print_vals(start, type,length);
 
     size_t size_low = (right - start)/type->size + 1;
     size_t size_high = (end - left)/type->size + 1;
@@ -112,7 +97,6 @@ void qs_task(char *start, ITYPE *type, size_t length) {
     qs_task(start, type, size_low);
 #pragma omp task untied
     qs_task(left, type, size_high);
-
   }
   else {
     qsort(start, length, type->size, type->compare);
@@ -130,8 +114,8 @@ size_t *prefix(size_t *counts, uint32_t length) {
     size_t *prefixSums = (size_t *) malloc(length*sizeof(size_t));
 
     size_t sum = 0;
-
     uint32_t i;
+
     for(i=0; i<length-1; i++) {
         prefixSums[i] = sum;
         sum += counts[i];
@@ -143,12 +127,8 @@ size_t *prefix(size_t *counts, uint32_t length) {
 
 size_t parallelPartition(void *input, ITYPE *type, size_t length, void *pivot, int threads) {
 
-//  printf("Pivot: %d\n", *((int *) pivot));
-
   int numThreads;
-
   size_t *counts;
-
   size_t split = 0;
   
 #pragma omp parallel num_threads(threads) default(none), shared(numThreads, split, counts) firstprivate(type, length, \
@@ -159,38 +139,25 @@ size_t parallelPartition(void *input, ITYPE *type, size_t length, void *pivot, i
 #pragma omp single
   {
     numThreads = omp_get_num_threads();
-//    printf("%d threads in partition, length: %lu\n", numThreads, length);
     counts = (size_t *) malloc(numThreads*sizeof(size_t));
-//    chunks = (CHUNK *) malloc((numThreads+1)*sizeof(CHUNK));
-//    chunks[numThreads].startIndex = length;
-//    chunks[numThreads].count_total = 0;
   }
 
   if(length > numThreads) { //shouldn't call it anyway otherwise, but so it won't fail...
   
     int tid = omp_get_thread_num();
 
-
-//    printf("[%f] Thread %d start.\n", omp_get_wtime(), tid);
-
     size_t itemsPerChunk = (length)/numThreads;  //last chunk larger...
     size_t startIndex = tid*itemsPerChunk;
     size_t chunklen = (tid == numThreads-1) ? length - tid*itemsPerChunk : itemsPerChunk;
 
-//    printf("Thread %d has [%lu:%lu] \n", tid, c->startIndex, c->startIndex + chunklen);
-
     char *buffer = (char *) malloc(chunklen*step);
-
     char *src = ((char *) input) + startIndex*step;
     char *end = src + chunklen*step;
-
     char *tgt_le = buffer;
     char *tgt_gt = tgt_le + (chunklen)*step;
     
     size_t countLE = 0;
-
-
-      
+     
     for(; src < end; src += step) {
       if(type->compare(src, pivot) > 0) {
         tgt_gt -= step;
@@ -204,26 +171,14 @@ size_t parallelPartition(void *input, ITYPE *type, size_t length, void *pivot, i
     }
 
     size_t countGT = chunklen - countLE;
-  
     counts[tid] = countLE;
-
-//    printf("[%f] Thread %d pass1 done.\n", omp_get_wtime(), tid);
   
 #pragma omp barrier
 
-//    printf("[%f] Thread %d pass2 start.\n", omp_get_wtime(), tid);
-
-    size_t * prefixSums = prefix(counts, numThreads+1); //parallel prefix doesn't really pay off with few processors...
-
+    size_t *prefixSums = prefix(counts, numThreads+1); //parallel prefix doesn't really pay off with few processors...
     size_t total_le = prefixSums[numThreads];
-
     size_t le_offset = prefixSums[tid];
     size_t gt_offset = total_le + (startIndex - prefixSums[tid]);
-
-
-  
-//    printf("LE: [%lu:%lu], GT: [%lu:%lu]\n", le_offset, le_offset+c->count_le, gt_offset, gt_offset + (c->count_total - c->count_le));
-
 
     char *src_le = buffer;
     char *src_gt = tgt_gt;
@@ -240,41 +195,31 @@ size_t parallelPartition(void *input, ITYPE *type, size_t length, void *pivot, i
     if(tid == numThreads - 1) {
       split = le_offset + countLE;
     }
-    
-//    printf("[%f] Thread %d done.\n", omp_get_wtime(), tid);
   }
   else  {
     #pragma omp single
     { 
       char *start = ((char *) input);
-
       char *left = start;
       char *right = start + (length-1)*step;
 
-//      printf("Start single partition, pivot %d\n", *((int *)pivot));
-//      print_ints0((int *) input, length);
-
       for(;;) {
-				while(left <= right && type->compare(left, pivot) <= 0) {
-						left += step;
-					}
-					while(right >= left && type->compare(right, pivot) > 0) {
-						right -= step;
-					}
-					if(left > right) {
-						break;
-					}
-					else {
-						swap(left, right, step);
+		while(left <= right && type->compare(left, pivot) <= 0) {
+			left += step;
+		}
+		while(right >= left && type->compare(right, pivot) > 0) {
+			right -= step;
+		}
+		if(left > right) {
+			break;
+		}
+		else {
+			swap(left, right, step);
             left += step;
             right -= step;
-					}
-				}
-
-				split = (left - start)/step;
-
-//        printf("End single partition, split: %lu\n", split);
-//        print_ints0((int *) input, length);
+		}
+	  }
+	  split = (left - start)/step;
     }
   }
 } //parallel
@@ -285,7 +230,6 @@ size_t parallelPartition(void *input, ITYPE *type, size_t length, void *pivot, i
 }
 
 static void qs(void *array, ITYPE *type, size_t length, int numThreads, uint32_t numPivots, int32_t rstate) {
-
 
   if(length < 2) {
     return;
@@ -304,37 +248,24 @@ static void qs(void *array, ITYPE *type, size_t length, int numThreads, uint32_t
 }
 
   if(threads < 2) {
-//    printf("T:%f Thread %d start seq sort (%lu).\n", omp_get_wtime(), omp_get_thread_num(), length);
-//    qsort(array, length, type->size, type->compare);
   #pragma omp task 
     qs_task((char *) array, type, length);
-//    printf("t:%f Thread %d seq sort done (%lu).\n", omp_get_wtime(), omp_get_thread_num(), length);
+
     return;
   }
 
-
-//  void *pivot = selectPivot(array, type, length);
   void *pivot = selectPivotRandom(array, type, length, numPivots, &rstate);
-
 
   swap((char *) array, (char *) pivot, type->size);
   pivot = array;
 
-//  printf("Before   : ");
-//  print_ints0((int *) array, length);
   left_size = parallelPartition(((char *) array) + type->size, type, length-1, pivot, threads);
-//  printf("Split %3lu: ", left_size);
-//  print_ints0((int *) array, length);
 
   swap((char *) pivot, ((char *) array) + left_size*type->size , type->size);
-
 
   double split = left_size/(double)length;
   int leftp = (int) (split*numThreads);
   if (leftp == 0) leftp++;
-
-//  printf("Split: %.2f (%d/%d)\n", split, leftp, numThreads-leftp);
-
 
 #pragma omp parallel sections
 {
@@ -347,11 +278,7 @@ static void qs(void *array, ITYPE *type, size_t length, int numThreads, uint32_t
   {
     qs(((char *) array) + (left_size+1)*type->size, type, length - left_size - 1, numThreads-leftp, numPivots, rstate);
   }
-}
-
-
- 
-  
+}  
   
 }
 
